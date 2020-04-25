549847c4b0a38772b98d47224258a3bdbc8edc8e
Orhungul Kelvin <kelvin.orhungul@africaprudential.com>
2020-04-25 20:57:35 +0100

    task(http-setup): setup axios for HTTP calls

---
+++ src/api/services.js
@@ -0,0 +1,4 @@
+import { AxiosInstance } from './AxiosInstance';
+import { covidAPIs } from './index';
+//we'll put the api calls here and just reference them in our component
+//we'll use the axios instance to make the calls(e.g AxiosInstance.get(covidAPIs.login))
